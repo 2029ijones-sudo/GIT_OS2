1d35556feff2c18e8aac6beaf9cb3c9a675931af
2029ijones-sudo <2029ijones@wfschools.org>
2026-02-11 11:29:08 -0600

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,7 +11,20 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Toaster position="top-right" richColors />
+      <Toaster 
+        position="top-right"
+        richColors
+        closeButton
+        theme="dark"
+        toastOptions={{
+          style: {
+            background: 'rgba(15, 23, 42, 0.95)',
+            backdropFilter: 'blur(12px)',
+            border: '1px solid rgba(255,255,255,0.1)',
+            color: 'white'
+          }
+        }}
+      />
     </>
   )
 }
